c67c08f9737fc75cb06f08308ee0f0a43a128f93
shhamil <kurban0595@inbox.ru>
2021-04-02 12:32:30 +0300

    postgresVPS

--- miniChat/settings.py
+++ miniChat/settings.py
@@ -85,24 +85,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'django_minichat',
-#        'USER': 'shamil',
-#        'PASSWORD': 'damgp0563',
-#        'HOST': 'localhost',
-#        'PORT': '5432',
-#
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_minichat',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
